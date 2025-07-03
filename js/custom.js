46eb007cfe4641ab916e5d130256cc02331dc68b
Stephen Suffian <stephen.suffian@pm.me>
2025-07-02 21:13:57 -0400

    Removed the loading screen

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,7 @@ jQuery(document).ready(function() {
     "use strict";
 
 
-/* ------- Preloader ------ */
-jQuery(window).load(function() {
-    jQuery(".status").fadeOut();
-    jQuery(".preloader").delay(1000).fadeOut("slow");
-});
+
 
 
 /* -------- Appears Menu ------ */
